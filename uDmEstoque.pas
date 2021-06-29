5379230da6d63384692097acd5b5f5c686b091bc
Gabriel Forner <gabrielforner11@gmail.com>
2021-06-29 20:13:34 -0300

    Criado método com sql para buscar dados na tabela estoque
    Criado método com sql para buscar dados na tabela estoque

--- uDmEstoque.pas
+++ uDmEstoque.pas
@@ -16,6 +16,7 @@ type
     { Private declarations }
   public
     { Public declarations }
+    procedure buscarEstoque(_descricao: string = '');
   end;
 
 var
@@ -26,5 +27,29 @@ implementation
 {%CLASSGROUP 'Vcl.Controls.TControl'}
 
 {$R *.dfm}
+
+{ TDmEstoque }
+
+procedure TDmEstoque.buscarEstoque(_descricao: string);
+begin
+  if not DmConexaoFB.Conexao.Connected then
+    DmConexaoFB.conectarBanco();
+
+    qrEstoque.Close();
+    qrEstoque.SQL.Clear();
+    qrEstoque.SQL.Add('select e.codigo,');
+    qrEstoque.SQL.Add('       e.barras,');
+    qrEstoque.SQL.Add('       e.descricao,');
+    qrEstoque.SQL.Add('       e.und,');
+    qrEstoque.SQL.Add('       e.und_compra,');
+    qrEstoque.SQL.Add('       e.qtd,');
+    qrEstoque.SQL.Add('       e.preco_custo,');
+    qrEstoque.SQL.Add('       e.preco_venda');
+    qrEstoque.SQL.Add('from estoque e');
+    qrEstoque.SQL.Add('where upper(e.descricao) containing upper(:descricao)');
+    qrEstoque.ParamByName('descricao').AsString = _descricao;
+    qrEstoque.Open();
+
+end;
 
 end.
