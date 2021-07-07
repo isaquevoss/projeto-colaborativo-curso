04ee6c35ce38b009c96445f6ae61f6154674bfd5
daltonlotti <82547754+daltonlotti@users.noreply.github.com>
2021-07-06 21:02:22 -0300

    Ajustado tela de busca do fornecedor

---
+++ uDmFornecedor.dfm
@@ -0,0 +1,5 @@
+object dmFornecedor: TdmFornecedor
+  OldCreateOrder = False
+  Height = 150
+  Width = 291
+end
---
+++ uDmFornecedor.pas
@@ -0,0 +1,25 @@
+unit uDmFornecedor;
+
+interface
+
+uses
+  System.SysUtils, System.Classes;
+
+type
+  TdmFornecedor = class(TDataModule)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  dmFornecedor: TdmFornecedor;
+
+implementation
+
+{%CLASSGROUP 'Vcl.Controls.TControl'}
+
+{$R *.dfm}
+
+end.
