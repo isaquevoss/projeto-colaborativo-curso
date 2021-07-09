86632d39929a9fe2f8d1b72041bd33f94ed0e4b5
Evertontwt <evertontwt@gmail.com>
2021-07-08 22:31:13 -0300

    Corrigidos erros de compilação

--- uDmTrasnportadora.dfm
+++ uDmTrasnportadora.dfm
@@ -3,6 +3,7 @@ object DmTrasnportadora: TDmTrasnportadora
   Height = 256
   Width = 324
   object qrUpdateInsert: TFDQuery
+    Connection = DmConexaoFB.Conexao
     Left = 144
     Top = 32
   end
--- uDmTrasnportadora.pas
+++ uDmTrasnportadora.pas
@@ -6,7 +6,7 @@ uses
   System.SysUtils, System.Classes, FireDAC.Stan.Intf, FireDAC.Stan.Option,
   FireDAC.Stan.Param, FireDAC.Stan.Error, FireDAC.DatS, FireDAC.Phys.Intf,
   FireDAC.DApt.Intf, FireDAC.Stan.Async, FireDAC.DApt, Data.DB,
-  FireDAC.Comp.DataSet, FireDAC.Comp.Client;
+  FireDAC.Comp.DataSet, FireDAC.Comp.Client,uDmConexaoFB;
 
 type
   TDmTrasnportadora = class(TDataModule)
@@ -15,7 +15,7 @@ type
     { Private declarations }
   public
     { Public declarations }
-    procedure SalvarTrasnportadora( _codigo : Integer;_razao:string; _endereco :string; _cidade : string; _uf : string; _telefone :string; _email: string; _cnpj: string; _rntrc:string; );
+    procedure SalvarTrasnportadora( _codigo : Integer;_razao:string; _endereco :string; _cidade : string; _uf : string; _telefone :string; _email: string; _cnpj: string; _rntrc:string );
   end;
 
 var
@@ -29,7 +29,7 @@ implementation
 
 { TDmTrasnportadora }
 
-procedure TDmTrasnportadora.SalvarTrasnportadora( _codigo : Integer;_razao:string; _endereco :string; _cidade : string; _uf : string; _telefone :string; _email: string; _cnpj: string; _rntrc:string;);
+procedure TDmTrasnportadora.SalvarTrasnportadora( _codigo : Integer;_razao:string; _endereco :string; _cidade : string; _uf : string; _telefone :string; _email: string; _cnpj: string; _rntrc:string);
 begin
   qrUpdateInsert.SQL.Clear();
   qrUpdateInsert.SQL.Add('INSERT INTO TRANSPORTE (CODIGO, RAZAO_SOCIAL, ENDERECO, CIDADE, UF, TELEFONE, EMAIL, CNPJ, IE, PLACA, UFV, RNTRC)');
