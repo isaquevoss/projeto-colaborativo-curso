b49f696dfef88cbaf4bbc7d09ab12750edaebee7
Evertontwt <evertontwt@gmail.com>
2021-07-15 22:56:49 -0300

    Ajuste validação campos

--- uCadastroEstoque.pas
+++ uCadastroEstoque.pas
@@ -44,13 +44,16 @@ end;
 
 procedure TfrmCadastroEstoque.btnGravarClick(Sender: TObject);
 var i : Integer;
+
 begin
   for I := 0 to ComponentCount -1 do
   begin
+
     if Components[i] is TEdit then
     if TEdit( Components[I] ).Text = '' then
     begin
-      ShowMessage('Existem campos em branco, verifique!'#13+ (Components[i]).Name);
+      ShowMessage('Existem campos em branco, verifique!'#13+ TEdit(Components[i]).TextHint);
+      TEdit(Components[i]).SetFocus;
           exit
     end;
 
