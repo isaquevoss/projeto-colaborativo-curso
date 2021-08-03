5ae3546c58955f9bd7294cd8935f21ab461229c6
Isaque Voss Klassmann <isaque.iv@gmail.com>
2021-08-03 19:41:30 -0300

    ajustado quebra de linha no sql do data module

--- uDmTransportadora.pas
+++ uDmTransportadora.pas
@@ -6,7 +6,7 @@ uses
   System.SysUtils, System.Classes, FireDAC.Stan.Intf, FireDAC.Stan.Option,
   FireDAC.Stan.Param, FireDAC.Stan.Error, FireDAC.DatS, FireDAC.Phys.Intf,
   FireDAC.DApt.Intf, FireDAC.Stan.Async, FireDAC.DApt, Data.DB,
-  FireDAC.Comp.DataSet, FireDAC.Comp.Client, uDmConexaoFB,uTransportadoraClasse;
+  FireDAC.Comp.DataSet, FireDAC.Comp.Client, uDmConexaoFB, uTransportadoraClasse;
 
 type
   TDmTransportadora = class(TDataModule)
@@ -17,9 +17,9 @@ type
     { Private declarations }
   public
     { Public declarations }
-      procedure Cadastrar(_transportadora :TTransportadora);
-      procedure CarregarTransportadora();
-      procedure BuscarTransp(_razao:string);
+    procedure Cadastrar(_transportadora: TTransportadora);
+    procedure CarregarTransportadora();
+    procedure BuscarTransp(_razao: string);
   end;
 
 var
@@ -37,28 +37,29 @@ implementation
 procedure TDmTransportadora.BuscarTransp(_razao: string);
 begin
   if not DmConexaoFB.Conexao.Connected then
-      DmConexaoFB.conectarBanco();
+    DmConexaoFB.conectarBanco();
 
   qrTransportadora.Close();
   qrTransportadora.SQL.Clear();
   qrTransportadora.SQL.Add('select * from transporte where upper(razao_social) like upper(:razao);');
-  qrTransportadora.ParamByName('razao').AsString := '%'+ _razao +'%';
+  qrTransportadora.ParamByName('razao').AsString := '%' + _razao + '%';
   qrTransportadora.Open();
-
 
 end;
 
-procedure TDmTransportadora.Cadastrar( _transportadora : TTransportadora);
+procedure TDmTransportadora.Cadastrar(_transportadora: TTransportadora);
 begin
   if not DmConexaoFB.Conexao.Connected then
-      DmConexaoFB.conectarBanco();
+    DmConexaoFB.conectarBanco();
 
   qrInsertUpdate.Close();
   qrInsertUpdate.SQL.Clear();
-  qrInsertUpdate.SQL.Add('INSERT INTO TRANSPORTE (CODIGO, RAZAO_SOCIAL, ENDERECO, CIDADE, UF, TELEFONE, EMAIL, CNPJ, RNTRC)');
+  qrInsertUpdate.SQL.Add('INSERT INTO TRANSPORTE ');
+  qrInsertUpdate.SQL.Add('(CODIGO, RAZAO_SOCIAL, ENDERECO, CIDADE, UF, TELEFONE, EMAIL, CNPJ, RNTRC)');
   qrInsertUpdate.SQL.Add('VALUES');
-  qrInsertUpdate.SQL.Add('((select case when max(transporte.codigo) is null then 1 else max(transporte.codigo)+1 end from transporte)'+
-  #13 + ',:RAZAO_SOCIAL, :ENDERECO, :CIDADE, :UF, :TELEFONE, :EMAIL, :CNPJ, :RNTRC)');
+  qrInsertUpdate.SQL.Add('((select case when max(transporte.codigo) is null then 1');
+  qrInsertUpdate.SQL.Add(' else max(transporte.codigo)+1 end from transporte)');
+  qrInsertUpdate.SQL.Add(',:RAZAO_SOCIAL, :ENDERECO, :CIDADE, :UF, :TELEFONE, :EMAIL, :CNPJ, :RNTRC)');
   qrInsertUpdate.ParamByName('RAZAO_SOCIAL').AsString := _transportadora.razao;
   qrInsertUpdate.ParamByName('ENDERECO').AsString := _transportadora.endereco;
   qrInsertUpdate.ParamByName('CIDADE').AsString := _transportadora.cidade;
@@ -74,11 +75,12 @@ end;
 procedure TDmTransportadora.CarregarTransportadora;
 begin
   if not DmConexaoFB.Conexao.Connected then
-      DmConexaoFB.conectarBanco();
+    DmConexaoFB.conectarBanco();
 
- qrTransportadora.Close();
- qrTransportadora.SQL.Clear();
- qrTransportadora.Open('Select * from transporte');
+  qrTransportadora.Close();
+  qrTransportadora.SQL.Clear();
+  qrTransportadora.Open('Select * from transporte');
 end;
 
 end.
+
